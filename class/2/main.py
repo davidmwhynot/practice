a4b498d57f8b9f8d1e2c9aef0773b3c5c2786788
davidmwhynot <davidmwhynot@gmail.com>
2018-01-26 13:04:28 -0500

    finished required coding assignment

--- class/2/main.py
+++ class/2/main.py
@@ -1,20 +1,45 @@
+x = [2,3,4,5]
+y = [1,2,4,5,6,7]
+
+
 def intersect(a, b):
 	buffer = []
 	for i in a:
 		for j in b:
-			if i == j
-				buffer.push(i)
+			if i == j:
+				buffer.append(i)
+	buffer.sort()
 	return buffer
+
+print(intersect(x,y))
+
 
 def union(a, b):
 	buffer = []
 	for i in a:
-		for j in b:
-			if i == j
-				buffer.push(i)
+		for k in buffer:
+			if i == k:
+				break
+		else:
+			buffer.append(i)
+	for i in b:
+		for k in buffer:
+			if i == k:
+				break
+		else:
+			buffer.append(i)
+	buffer.sort()
 	return buffer
+
+print(union(x,y))
+
 
 def equal(a, b):
 	for i in a:
 		for j in b:
-			if i != j
+			if i != j:
+				return False
+	else:
+		return True
+
+print(equal(x,y))
